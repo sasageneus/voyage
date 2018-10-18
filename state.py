4233535a8196f37f6b6e20248ada8bd44633d66c
sasa-pc <sasa@email.ru>
2018-10-18 21:44:02 +0300

    v1018 from work

--- state.py
+++ state.py
@@ -1,2 +1,28 @@
 import table
 
+class State:
+    tbl = None #таблица расстояний (приведенных к нулю)
+    n = None #количество пуктов назначения
+    cost = None #оценка минимально возможного маршрута это его расстояние определяется как Sum(min_from + min_to)
+    min_from = [] #соответсвует строке i и содержит минимальное рассотояние из пункта i
+    min_to = [] #соответсвует столбцу j и содержит минимальное добавочное рассотояние до пункта j 
+
+    def __init__(self, tbl):
+        self.tbl = tbl;
+        n = len(tbl)
+        
+        for i in range(n):
+            self.min_from.append(0)
+            self.min_to.append(0)
+
+        
+
+    def define_cost(self):
+        
+
+def unittest():
+    tbl = simple_generator_distance_table(5, 9))
+
+    
+
+    print_distance(tbl)
--- table.py
+++ table.py
@@ -1,10 +1,10 @@
-
 import random
 
-def set_distance(value, used = True, worse = 0):
+def distance_of(value, used = True, worse = 0):
     return (value, used, worse)
 
-def simple_generator_distance_table(n, max_distance):
+#Генерирует случайные расстояния между точками
+def generate_random_distance(n, max_distance):      
     tbl = []
     for i in range(n):
         tbl.append([])
@@ -12,9 +12,9 @@ def simple_generator_distance_table(n, max_distance):
             tbl[i].append([])
 
     for i in range(n):
-        tbl[i][i] = set_distance('-', False)
+        tbl[i][i] = distance_of('-', False)
         for j in range(i + 1, n):
-            tbl[i][j] = set_distance(random.randint(1, max_distance))
+            tbl[i][j] = distance_of(random.randint(1, max_distance))
             tbl[j][i] = tbl[i][j]
 
     return tbl
@@ -25,6 +25,87 @@ def print_distance(tbl):
             v, u, w = tbl[i][j]
             print('%s' % str(v), end='\t')
         print('')
+    print('')
+
+def get_column(tbl, j):
+    col = []
+    for i in range(len(tbl)):
+        col.append(tbl[i][j])
+    
+    return col
+
+def set_column(tbl, j, column):
+    for i in range(len(tbl)):
+        tbl[i][j] = column[i]
+
+def column_test():
+    tbl = generate_random_distance(5, 9)
+    print_distance(tbl)    
+    col3 = get_column(tbl, 3)
+    col1 = get_column(tbl, 1)
+    #for i in range(len(tbl)):
+     #   col[i] = distance_of(999)
+    set_column(tbl, 1, col3)
+    set_column(tbl, 3, col1)
+    print_distance(tbl)    
+
+#column_test()
+
+def minimaze(row):
+    #min_value = min([dist[0] for dist in row if dist[1] ]) 
+    # находим минимальный при этом пропускаем неиспользуемые
+    min_value = None
+    for dist in row:
+        v, u, w = dist
+        if u and (min_value == None or v < min_value):
+            min_value = v
+
+    if min_value == None:
+        min_value = 0 #Если все неиспользуются то 0 чтобы не повлиять на общую оценку
+
+    #Нормализуем, путем вычитания минимума из всех используемых ячеек
+    ret_row = []
+    for dist in row:
+        v, u, w = dist
+        if u:
+            ret_row.append(distance_of(v - min_value, u, w))
+        else:
+            ret_row.append(dist)
+
+    return ret_row, min_value
+
+def minimaze_test():
+    tbl = generate_random_distance(5, 9)
+    print_distance(tbl)    
+    col, min_val = minimaze(get_column(tbl, 1))
+    set_column(tbl, 1, col)
+    print(min_val)
+    print_distance(tbl)    
+
+minimaze_test()
+
+def copy(tbl):
+    new_tbl = tbl.copy()
+    for i in range(len(new_tbl)):
+        new_tbl[i] = new_tbl[i].copy()
+
+    return new_tbl
 
 
-print_distance(simple_generator_distance_table(5, 9))
+def copy_test():
+    tbl = generate_random_distance(5, 9)
+
+    new_tbl = copy(tbl)    
+    assert(tbl[1][2] == new_tbl[1][2])
+
+    new_tbl[1][2] = distance_of(*new_tbl[1][2])
+    assert(tbl[1][2] == new_tbl[1][2])
+
+    new_tbl[1][2] = distance_of(99)
+    assert(tbl[1][2] != new_tbl[1][2])
+
+    print_distance(tbl)    
+    print('copy table:')
+    print_distance(new_tbl)
+    
+#copy_test()
